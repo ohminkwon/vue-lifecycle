2896bc8ff10f4d67a71aa69b5966c7161ac30f84
ohminkwon <ohminkwon0720@gmail.com>
2021-09-06 22:34:53 +0900

    setting vue3 and testing life-cycle

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <input
+    type="text"
+    v-model.trim="msg" />
+
+  <h1>{{ checked }}</h1>
+  <input
+    type="checkbox"
+    v-model="checked" />
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg: 'Hello World!',
+      checked: false
+    }
+  },
+  watch:{
+    msg(){
+      console.log(this.msg)
+    }
+  }
+}
+</script>
---
+++ src/components/Fruits.vue
@@ -0,0 +1,45 @@
+<template>
+  <section v-if="hasFruit">
+    <h1>Fruits</h1>
+    <ul>
+      <li
+        v-for="fruit in fruits"
+        :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+  <section>
+    <h1>Reverse Fruits</h1>
+    <ul>
+      <li
+        v-for="fruit in reverseFruit"
+        :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      fruits: [
+        'Apple', 'Banana', 'Cherry'
+      ]
+    }
+  },
+  computed:{
+    hasFruit(){
+      return this.fruits.length > 0
+    },
+    reverseFruit(){
+      return this.fruits.map(fruit=>{
+        // 'Apple' => ['A', 'p', 'p', 'l', 'e'] => ['e', 'l', 'p', 'p', 'A']
+        return fruit.split('').reverse().join('')
+      })
+    }
+  }
+}
+</script>
